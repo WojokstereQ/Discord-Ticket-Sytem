eea9ba1ad5966448a00afb24e588cce7d3982454
WojokstereQ <166377254+WojokstereQ@users.noreply.github.com>
2024-10-24 00:01:13 +0200

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     modmail: {
-        ownerID: '', // ← Insert the Server Owner ID.
-        supportId: '', // ← Insert General Support Category ID.
-        premiumId: '', // ← Insert Premium Support Category ID.
-        whitelist: '', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
-        staff: '', // ← Insert Staff Role to be pinged upon ticket creation.
+        ownerID: '477080922366148629', // ← Insert the Server Owner ID.
+        supportId: '1129469707578265710', // ← Insert General Support Category ID.
+        premiumId: '1298767365164634195', // ← Insert Premium Support Category ID.
+        whitelist: '1129469693233737918', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        staff: '1129469693233737918', // ← Insert Staff Role to be pinged upon ticket creation.
     },
     logs: {
         logschannel: '' // ← Insert Ticket Logs Channel ID.
